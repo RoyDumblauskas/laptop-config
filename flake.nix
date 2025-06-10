0b096a33bc9d877877dee01c639840065b72ddd3
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-10 08:55:02 -0500

    add readme

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
+    # implement at some point (So I can push repo public)
     sops-nix = {
 	    url = "github:Mic92/sops-nix";
 	    inputs.nixpkgs.follows = "nixpkgs";
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Roy's Laptop Config
+
+## Todo:
+
+- Implement sops for secrets
+- Change repo to public (Make sure to squash to delete secret history)
