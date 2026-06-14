bb76bf877970dfbaa48a829111ae308f12f1baaa
Roy Dumblauskas <roydumblauskas@gmail.com>
2026-06-14 17:37:53 -0500

    no vim.lsp.with()

--- home.nix
+++ home.nix
@@ -545,23 +545,24 @@
       };
     };
     extraConfigLua = ''
-            local _border = "rounded"
+      local _border = "rounded"
 
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-              vim.lsp.handlers.hover, {
-                border = _border
-              }
-            )
+      vim.lsp.config('*', {
+        handlers = {
+          ["textDocument/hover"] = {
+            border = _border,
+          },
+          ["textDocument/signatureHelp"] = {
+            border = _border,
+          },
+        },
+      })
 
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-              vim.lsp.handlers.signature_help, {
-      	        border = _border
-      	      }
-            )
-
-            vim.diagnostic.config{
-            	float={border=_border}
-            };
+      vim.diagnostic.config({
+        float = {
+          border = _border,
+        },
+      })
     '';
 
     colorschemes.catppuccin = {
